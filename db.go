ecb4a939d78bf56506203a1de2f4af3244eef40d
go-while <go-while@users.noreply.github.com>
2023-10-24 18:45:07 +0200

    gets a diff
    2023/10/24 18:44:44 1698165884676286942 !9! b.db.batch.id=26 != b.id=25 diff=1 ???
    2023/10/24 18:44:44 1698165884686400208 !9! b.db.batch.id=31 != b.id=19 diff=12 ???
    2023/10/24 18:44:44 1698165884723785458 !9! b.db.batch.id=43 != b.id=42 diff=1 ???
    2023/10/24 18:44:44 1698165884731057101 !9! b.db.batch.id=35 != b.id=21 diff=14 ???
    2023/10/24 18:44:44 1698165884732422160 !9! b.db.batch.id=33 != b.id=19 diff=14 ???
    2023/10/24 18:44:44 1698165884748080688 !9! b.db.batch.id=36 != b.id=24 diff=12 ???
    2023/10/24 18:44:44 1698165884753737246 !9! b.db.batch.id=38 != b.id=20 diff=18 ???
    
    ...
    2023/10/24 18:44:44 1698165884936362231 !9! b.db.batch.id=55 != b.id=54 diff=1 ???
    2023/10/24 18:44:44 1698165884943496866 !9! b.db.batch.id=56 != b.id=55 diff=1 ???
    2023/10/24 18:44:44 1698165884980214101 !9! b.db.batch.id=67 != b.id=66 diff=1 ???
    2023/10/24 18:44:44 1698165884983061195 !9! b.db.batch.id=58 != b.id=48 diff=10 ???
    2023/10/24 18:44:44 1698165884986818580 !9! b.db.batch.id=56 != b.id=44 diff=12 ???
    2023/10/24 18:44:45 1698165885019473395 !9! b.db.batch.id=69 != b.id=49 diff=20 ???

--- db.go
+++ db.go
@@ -1219,7 +1219,7 @@ func (b *batch) run() {
 
 		/* SHORTER DEBUG */
 		if b.db.batch != nil && b != nil {
-			log.Printf("%d !9! b.db.batch.id=%d != b.id=%d ???", trace, b.db.batch.id, b.id)
+			log.Printf("%d !9! b.db.batch.id=%d != b.id=%d diff=%d ???", trace, b.db.batch.id, b.id, b.db.batch.id-b.id)
 		}
 
 		//os.Exit(121)
